b53b66c0383b233331114ea62ad36f0f9e351b6f
Dylan Araps <dylan.araps@gmail.com>
2021-08-13 13:22:58 +0300

    ah yes

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,5 @@ fn main() {
     let config: I18nConfig = I18nConfig{locales: &["en", "es"], directory: "translations"};
     let r_i18n: I18n = I18n::configure(&config);
 
-    unsafe {
-        let text = r_i18n.t("msg");
-
-        println!("{}", text);
-    }
+    unsafe { println!("{}", r_i18n.t("msg")); }
 }
